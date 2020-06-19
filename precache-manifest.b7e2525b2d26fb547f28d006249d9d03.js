b136c13fa2fe30a7c9f989dbbe8a6e3873862212
carlosziegler <carlos.ziegler@gmail.com>
2020-06-19 14:36:22 +0200

    Updates

---
+++ precache-manifest.b7e2525b2d26fb547f28d006249d9d03.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2c7af352931a6ec043b017c4214ea73",
+    "url": "/recycling-game/index.html"
+  },
+  {
+    "revision": "623c4cfb00b61869e14e",
+    "url": "/recycling-game/static/css/main.cbd176d3.chunk.css"
+  },
+  {
+    "revision": "0d2d55c0cb3c8b5b3ad7",
+    "url": "/recycling-game/static/js/2.11008df7.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/recycling-game/static/js/2.11008df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "623c4cfb00b61869e14e",
+    "url": "/recycling-game/static/js/main.10231889.chunk.js"
+  },
+  {
+    "revision": "0e15799d3decd8d2f4ef",
+    "url": "/recycling-game/static/js/runtime-main.d7489810.js"
+  },
+  {
+    "revision": "126c620ffbdcbb1598389a6134b621f7",
+    "url": "/recycling-game/static/media/Random-Image.126c620f.png"
+  },
+  {
+    "revision": "ad35724ecf62606ac1a630f88d722353",
+    "url": "/recycling-game/static/media/Random-Image1.ad35724e.png"
+  },
+  {
+    "revision": "64337a139d9750b07f6082802d983106",
+    "url": "/recycling-game/static/media/Random-Image10.64337a13.png"
+  },
+  {
+    "revision": "50e1ffcbfa1c80e13ba33eb73a3fe695",
+    "url": "/recycling-game/static/media/Random-Image11.50e1ffcb.png"
+  },
+  {
+    "revision": "0d2e6c38dbee1817170beca67406df86",
+    "url": "/recycling-game/static/media/Random-Image12.0d2e6c38.png"
+  },
+  {
+    "revision": "a6245baf5bc0f4fd5b21e51fb44dd119",
+    "url": "/recycling-game/static/media/Random-Image13.a6245baf.png"
+  },
+  {
+    "revision": "8cb8b24fa766fc1828e81022850abd7a",
+    "url": "/recycling-game/static/media/Random-Image14.8cb8b24f.png"
+  },
+  {
+    "revision": "d208a4c1657e3f9e402cd106ad2b2ac2",
+    "url": "/recycling-game/static/media/Random-Image15.d208a4c1.png"
+  },
+  {
+    "revision": "0acf8aa73e933053c7f567e4e14ce872",
+    "url": "/recycling-game/static/media/Random-Image16.0acf8aa7.png"
+  },
+  {
+    "revision": "1b54cafaeb5ecdb66611894c4428bbe5",
+    "url": "/recycling-game/static/media/Random-Image17.1b54cafa.png"
+  },
+  {
+    "revision": "13fa13500b046ec960d24443daf71e7e",
+    "url": "/recycling-game/static/media/Random-Image2.13fa1350.png"
+  },
+  {
+    "revision": "71d7f27ab0984133f174db7ee08a0da3",
+    "url": "/recycling-game/static/media/Random-Image3.71d7f27a.png"
+  },
+  {
+    "revision": "bdf78c12fa14e0432186995478121b78",
+    "url": "/recycling-game/static/media/Random-Image4.bdf78c12.png"
+  },
+  {
+    "revision": "7967222647f77ded588cd342744dbe66",
+    "url": "/recycling-game/static/media/Random-Image5.79672226.png"
+  },
+  {
+    "revision": "2ba8c4c379491e6103c7e5660461f48f",
+    "url": "/recycling-game/static/media/Random-Image6.2ba8c4c3.png"
+  },
+  {
+    "revision": "449d2d0cb44848967193fa80280805e7",
+    "url": "/recycling-game/static/media/Random-Image7.449d2d0c.png"
+  },
+  {
+    "revision": "f4bb17761c827480efeb41584b181c4d",
+    "url": "/recycling-game/static/media/Random-Image8.f4bb1776.png"
+  },
+  {
+    "revision": "5596f4296512f9f1c676dc9c5ca6a100",
+    "url": "/recycling-game/static/media/Random-Image9.5596f429.png"
+  },
+  {
+    "revision": "188c3e4acd8a47108fe7878195770cf0",
+    "url": "/recycling-game/static/media/blue.188c3e4a.jpg"
+  },
+  {
+    "revision": "d29296737952728494b8d8773e037cb6",
+    "url": "/recycling-game/static/media/bodyBlue.d2929673.png"
+  },
+  {
+    "revision": "df22e13e91ba6c92dd3886c17410cea9",
+    "url": "/recycling-game/static/media/bodyBrown.df22e13e.png"
+  },
+  {
+    "revision": "7992e89dcd75f82f30892b0cd971a7ca",
+    "url": "/recycling-game/static/media/bodyGreen.7992e89d.png"
+  },
+  {
+    "revision": "d3ed378464154fae1361425c62da5286",
+    "url": "/recycling-game/static/media/bodyGrey.d3ed3784.png"
+  },
+  {
+    "revision": "4457ed98fb2848a3c6aa3faff47f295b",
+    "url": "/recycling-game/static/media/bodyYellow.4457ed98.png"
+  },
+  {
+    "revision": "08fbea45090726bf4553bc28e22c2efc",
+    "url": "/recycling-game/static/media/brown.08fbea45.png"
+  },
+  {
+    "revision": "e771599a84c3e16721d9b421019209ae",
+    "url": "/recycling-game/static/media/green.e771599a.png"
+  },
+  {
+    "revision": "bba7598a24ffac2bc3f29e2c72959a8f",
+    "url": "/recycling-game/static/media/grey.bba7598a.png"
+  },
+  {
+    "revision": "3afef00148c88dbe09726ded93321e8c",
+    "url": "/recycling-game/static/media/yellow.3afef001.png"
+  }
+]);
